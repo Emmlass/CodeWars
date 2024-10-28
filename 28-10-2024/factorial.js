8b060bf5932770863437caa0a2f376da63cbf402
Emmlass <enaibreemma@gmail.com>
2024-10-28 12:52:45 +0100

    Refactoring my code to show my though process

--- 28-10-2024/factorial.js
+++ 28-10-2024/factorial.js
@@ -7,8 +7,15 @@ https://en.wikipedia.org/wiki/Factorial
 
 */
 
+// Take Number integers as parameter
+//returns the factorial of the number
+// eg 5 factorial should give 120
+
+console.log(factorial(5), 120)
+console.log(factorial(6), 720)
 function factorial(n) {
   //your code here
+
   let result = 1;
   for (let i = n; n > 1; n--) {
     result *= n;
