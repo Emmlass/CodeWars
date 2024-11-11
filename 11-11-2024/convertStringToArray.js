cfb663c0b13decad86492c77e71362bfcecab3c0
Emmlass <enaibreemma@gmail.com>
2024-11-11 09:09:49 +0100

    Simple code challenge for today

---
+++ 11-11-2024/convertStringToArray.js
@@ -0,0 +1,17 @@
+/*
+Write a function to split a string and convert it into an array of words.
+
+Examples (Input ==> Output):
+"Robin Singh" ==> ["Robin", "Singh"]
+
+"I love arrays they are my favorite" ==> ["I", "love", "arrays", "they", "are", "my", "favorite"]
+Arrays  Strings  Fundamentals
+*/
+
+// Takes in a string of a sentence
+//returns an array of words of the sentence
+
+function stringToArray(string) {
+  // code code code
+  return string.split(" ");
+}
