e4558de9834e32b06a206e954bccf43cda0d919a
Emmlass <enaibreemma@gmail.com>
2024-12-31 04:14:01 +0100

    Multiplication table Solution

--- 2024-12-31/multiplicationtable.js
+++ 2024-12-31/multiplicationtable.js
@@ -9,5 +9,13 @@ For example, when given size is 3:
 For the given example, the return value should be:
 
 [[1,2,3],[2,4,6],[3,6,9]]
- */
 
+link: https://www.codewars.com/kata/534d2f5b5371ecf8d2000a08/train/javascript
+ */ 
+
+// an array length of the size {N}  
+// the array collects other arrays of rows that put in the multiplication of the size of N
+
+// returns an array of the multiplication of N
+
+console.log()
