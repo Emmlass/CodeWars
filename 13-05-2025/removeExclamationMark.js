b61a17fdf89e7df3a6aef0c59759d3f9bed4a8c9
Emmlass <enaibreemma@gmail.com>
2025-05-13 12:29:20 +0100

    To remove exclamation becomes very easy without using RegEx

---
+++ 13-05-2025/removeExclamationMark.js
@@ -0,0 +1,10 @@
+/*
+Write function RemoveExclamationMarks which removes all exclamation marks from a given string.
+
+FundamentalsStrings
+*/
+
+function removeExclamationMarks(s) {
+  //let exclaim = (/[!]/gmi);
+  return s.replace("!","")
+}
