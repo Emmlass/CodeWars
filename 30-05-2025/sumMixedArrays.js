ba6cf78863ddadb4d1fce3b39c5cbc74e8857179
Emmlass <enaibreemma@gmail.com>
2025-06-02 09:35:22 +0100

    "Used For loop to solve the problem of summing mixed arrays in JavaScript. The function iterates through each element, checks its type, and sums the numbers while ignoring non-numeric values."

--- 30-05-2025/sumMixedArrays.js
+++ 30-05-2025/sumMixedArrays.js
@@ -4,6 +4,15 @@ Given an array of integers as strings and numbers, return the sum of the array v
 Return your answer as a number.
 */
 
-let sumMixed = (x)=>{
-    
-}
+let sumMix = (x)=>{
+    let arr = 0;
+    for(let i = 0 ; i <x.length; i+=1){
+        x[i] = Number(x[i])
+        arr += x[i]        
+    }
+    return arr
+    }
+
+console.log(sumMix([9, 3, '7', '3']), 22);
+console.log(sumMix(['5', '0', 9, 3, 2, 1, '9', 6, 7]), 42);
+console.log(sumMix(['3', 6, 6, 0, '5', 8, 5, '6', 2,'0']), 41);
